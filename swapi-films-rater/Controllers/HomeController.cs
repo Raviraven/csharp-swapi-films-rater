97e54aae8a78f709d4f01044f049096193efc8da
Michał Kruszewski <kruszewski2@gmail.com>
2020-12-16 22:56:17 +0100

    Index html - table with films list - implemented

--- swapi-films-rater/Controllers/HomeController.cs
+++ swapi-films-rater/Controllers/HomeController.cs
@@ -1,26 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using swapi_films_rater.Models;
+using swapi_films_rater.Repository.APIServices;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using swapi_films_rater.Repository.Helpers;
 
 namespace swapi_films_rater.Controllers
 {
     public class HomeController : Controller
     {
+        private IFilmsSwapiService _filmsSwapiService { get; set; }
+        private FilmsHelper FilmsService { get; set; }
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IFilmsSwapiService filmsSwapiService,
+            FilmsHelper filmsService)
         {
             _logger = logger;
+            _filmsSwapiService = filmsSwapiService;
+            FilmsService = filmsService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var filmsContainer = await _filmsSwapiService.Get();
+            var dropDownList = FilmsService.GetTitlesAndEpisodes(filmsContainer.Results);
+
+            return View(dropDownList);
         }
 
         public IActionResult Privacy()
