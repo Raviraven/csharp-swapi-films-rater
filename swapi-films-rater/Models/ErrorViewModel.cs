682e426781e0cdce6d3503a99b7b259269f40294
Michał Kruszewski <kruszewski2@gmail.com>
2020-12-16 21:23:18 +0100

    Initial commit

---
+++ swapi-films-rater/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace swapi_films_rater.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
